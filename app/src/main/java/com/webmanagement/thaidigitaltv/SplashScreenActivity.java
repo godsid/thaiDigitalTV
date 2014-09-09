ccbb381230cd1dfccfeea69bb0981ff9547fb54a
Banpot Srihawong <banpot.sr@gmail.com>
2014-09-09 15:10:53 +0700

    init Ptoject

---
+++ app/src/main/java/com/webmanagement/thaidigitaltv/SplashScreenActivity.java
@@ -0,0 +1,48 @@
+package com.webmanagement.thaidigitaltv;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreenActivity extends Activity {
+
+    Handler handler;
+    Runnable runnable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, 3000);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(runnable);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+
+    }
+}
