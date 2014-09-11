fea631280d1a5523ac2d1593902be3d949615fde
whitecrows <executives_23@hotmail.com>
2014-09-11 09:41:48 +0700

    first page left navigation drawer

--- app/src/main/java/com/webmanagement/thaidigitaltv/FragmentTwo.java
+++ app/src/main/java/com/webmanagement/thaidigitaltv/FragmentTwo.java
@@ -1,7 +1,40 @@
 package com.webmanagement.thaidigitaltv;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by WhiteCrows on 9/10/2014.
  */
-public class FragmentTwo {
+public class FragmentTwo extends Fragment {
+    ImageView ivIcon;
+    TextView tvItemName;
+
+    public static final String IMAGE_RESOURCE_ID = "iconResourceID";
+    public static final String ITEM_NAME = "itemName";
+
+    public FragmentTwo() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_layout_two, container,
+                false);
+
+        ivIcon = (ImageView) view.findViewById(R.id.frag2_icon);
+        tvItemName = (TextView) view.findViewById(R.id.frag2_text);
+
+        tvItemName.setText(getArguments().getString(ITEM_NAME));
+        ivIcon.setImageDrawable(view.getResources().getDrawable(
+                getArguments().getInt(IMAGE_RESOURCE_ID)));
+        return view;
+    }
 }
